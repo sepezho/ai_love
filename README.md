fa6ec4ca8131e27661fdf5e9a1c8fdceb4f45efc
sepezho <sepezho@gmail.com>
2024-02-18 10:50:38 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>this is ai tg bot for cronjob that will do one thing and then turn off</h1>
+-------------------<br />
+Dependencies:<br />
+telebot;<br />
+openai;<br />
+-------------------<br />
+	
---
+++ ai_bot.py
@@ -0,0 +1,29 @@
+# This Python file uses the following encoding: utf-8
+import telebot
+import openai 
+from envs import openaikey, proxy_url, telekey
+
+openai.api_key = openaikey 
+bot = telebot.TeleBot(telekey)
+
+@bot.message_handler(commands=['start'])
+def message_handler_start_main(message):
+	msg = bot.send_message(message.chat.id, 'hey hi')
+	return
+
+prompt = "придумай локанчиный и короткий комплимент в одно предложение напиши только его."
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo-0301",
+    messages=[
+            {"role": "system", "content": "You are a chatbot"},
+            {"role": "user", "content": prompt},
+        ]
+)
+
+result = ''
+for choice in response.choices:
+    result += choice.message.content
+bot.send_message(707939820, result)
+
+# bot.polling()
